8de0bf28c488eb5d87d51ebef4609f05dfd1c4cb
Kate Turalnikova <turalnikova@gmail.com>
2019-12-29 20:38:23 +0200

    create artist directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import artistDAta from './mock_data/artistdata';
+
+export class Artist {
+  name: string;
+  shortname?: string;
+  reknown?: string;
+  bio?: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'skiDayAppTest';
+  name: string;
+  artists: Artist[];
+  currentArtist: Artist;
+
+  constructor() {
+    this.name = 'Hassum Harold';
+    this.artists = artistDAta;
+  }
+
+  addArtist(artist) {
+    this.artists.push(
+      {
+        name: artist,
+        reknown: 'Penn State',
+      }
+    );
+  }
+
+  onClick(artist, artistContainer: HTMLLIElement) {
+    this.name = artist.name;
+    artistContainer.style.backgroundColor = '#FECE4E';
+  }
 }
